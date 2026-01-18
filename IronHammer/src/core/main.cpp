450df4f7326764167dfced21362cb8a2ddb908c2
Arya-H03 <Arya-H03@users.noreply.github.com>
2026-01-17 23:51:24 -0500

    Initial Project Upload
    Added CMake and folder structure

---
+++ IronHammer/src/core/main.cpp
@@ -0,0 +1,36 @@
+﻿#include "main.h"
+#include <SFML/Graphics.hpp>
+#include <imgui.h>
+#include <imgui-SFML.h>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode({ 1280, 720 }), "IronHammer");
+    window.setFramerateLimit(60);
+
+    ImGui::SFML::Init(window);
+
+    sf::Clock deltaClock;
+    while (window.isOpen())
+    {
+        while (const auto event = window.pollEvent())
+        {
+            ImGui::SFML::ProcessEvent(window, *event);
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        ImGui::SFML::Update(window, deltaClock.restart());
+
+        ImGui::Begin("Debug");
+        ImGui::Text("ImGui + SFML 3 works");
+        ImGui::End();
+
+        window.clear();
+        ImGui::SFML::Render(window);
+        window.display();
+    }
+
+    ImGui::SFML::Shutdown();
+}
+
